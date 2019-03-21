197f10405783c8d5203a892ad869a8e0e78bedce
Pavel Milanes (CO7WT) <dev0032@simelo.tech>
2019-03-21 01:02:20 -0400

    Refs #13, remove a unneeded part in a path

--- build.sh
+++ build.sh
@@ -654,7 +654,7 @@ function calc_sums_compress() {
 function skywire_compile() {
     # make skywire under travis PC
     cd ${FS_MNT_POINT}${SKYWIRE_DIR}
-    sudo env GOPATH="${FS_MNT_POINT}${GOPATH}/go" OPTS="GO111MODULE=on GOOS=linux GOARCH=arm64" make
+    sudo env GOPATH="${FS_MNT_POINT}${GOPATH}" OPTS="GO111MODULE=on GOOS=linux GOARCH=arm64" make
     tree ${FS_MNT_POINT}${GOPATH}/go
 
     disable_chroot
