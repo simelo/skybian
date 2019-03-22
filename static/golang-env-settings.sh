2b332df9fa13ffdf6217778acc418cb9470e990e
Pavel Milanes (CO7WT) <dev0032@simelo.tech>
2019-03-22 11:09:26 -0400

    Refs #13, updating the script that sets the main skuwire env vars

--- static/golang-env-settings.sh
+++ static/golang-env-settings.sh
@@ -3,23 +3,27 @@
 # set golang path on the environment for the user, just for the user.
 
 # if there is a /etc/default/skywire file load it from there
-if [ -f "/etc/default/skywire" ] ; then
-    # good, load from skywire file
-    . "/etc/default/skywire"
-else
-    # nope, set it manual
-    HOME=/root
-    GOROOT=/usr/local/go
-    SKYCOIN_DIR=/usr/local/skycoin
-    GOPATH=${SKYCOIN_DIR}/go
-    SKYWIRE_DIR=${SKYCOIN_DIR}/skywire
-    SKYWIRE_DATA=${HOME}/.skycoin/skywire/
-    PATH=$PATH:$GOPATH/bin/:$GOROOT/bin/
-    export HOME
-    export GOROOT
-    export GOPATH
-    export SKYCOIN_DIR
-    export SKYWIRE_DIR
-    export SKYWIRE_DATA
-    export PATH
+if [ ! -f "/etc/default/skywire" ] ; then
+    # not there, set it manual
+    #### ! Warning the content in the echo statement need to be explicit
+    ####   with no var expansion
+    echo "
+HOME=/root
+GOROOT=/usr/local/go
+SKYCOIN_DIR=/usr/local/skycoin
+GOPATH=/usr/local/skycoin/go
+SKYWIRE_DIR=/usr/local/skycoin/skywire
+SKYWIRE_DATA=/root/.skycoin/skywire/
+PATH=${PATH}:/usr/local/skycoin/go/bin/:/usr/local/go/bin/
+export HOME
+export GOROOT
+export GOPATH
+export SKYCOIN_DIR
+export SKYWIRE_DIR
+export SKYWIRE_DATA
+export PATH" > /etc/default/skywire
+
 fi
+
+# load skywire default paths onto the environment
+. "/etc/default/skywire"
